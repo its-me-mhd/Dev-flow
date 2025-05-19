8fa372e549fe4dc43852cade4396f4b0c3b4c8d9
it's me <mohamedabdirisaaq57@gmail.com>
2025-05-19 08:37:38 +0600

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,32 +1,21 @@
 import { authMiddleware } from "@clerk/nextjs";
 
 export default authMiddleware({
- 
   publicRoutes: [
     "/",
-    "/api/webhook",
-
-    'question/:id',
-
-    '/tags',
-    '/tags/:id',
-    '/profile/:id',
-    '/community',
-    '/jobs'
-  
-  ], 
-  ignoredRoutes:[
-    "/api/webhook",
-    '/api/chatgtp',
-   
-
-  ]
+    "/api/webhook", 
+    "/question/:id*", 
+    "/tags",
+    "/tags/:id*",
+    "/profile/:id*",
+    "/community",
+    "/jobs",
+  ],
+  ignoredRoutes: [
+    "/api/chatgpt", 
+  ],
 });
 
- 
-
- 
 export const config = {
-  matcher: ['/((?!.+\\.[\\w]+$|_next).*)', '/', '/(api|trpc)(.*)'],
+  matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
 };
- 
